9d8196f8fd0aa40c994b8eea3b80ab4d173a70a9
Kyle Soliveres <kyle.mngbt@gmail.com>
2024-09-19 19:43:55 +0800

    feat: allow 2 digits date to be displayed properly

--- javascript/script.js
+++ javascript/script.js
@@ -8,7 +8,7 @@ const today = new Date();
 const weekDays = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
 const allMonths = ["January","February","March","April","May","June","July","August","September","October","November","December"];
 
-date.textContent = today.getDate();
+date.textContent = (today.getDate() < 10 ? "0" : "") + today.getDate();
 day.textContent = weekDays[today.getDay()];
 month.textContent = allMonths[today.getMonth()];
 year.textContent = today.getFullYear();
