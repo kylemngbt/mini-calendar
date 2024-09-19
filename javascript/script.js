2c961cfc79cadcdb0a590da73f6e8574f878438c
Kyle Soliveres <kyle.mngbt@gmail.com>
2024-09-19 19:35:49 +0800

    build: create four references

---
+++ javascript/script.js
@@ -0,0 +1,4 @@
+const date = document.querySelector("#date");
+const day = document.querySelector("#day");
+const month = document.querySelector("#month");
+const year = document.querySelector("#year");
